fa4fe22a43e8a08407f27b081dafad9fcc4183d7
efrat-packter <EFRATPACKTER@GMAIL.COM>
2026-03-13 13:59:58 +0200

    calc expression fromm lisp format

---
+++ Parser/Parse.cs
@@ -0,0 +1,39 @@
+﻿using ExpressionCalculation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parser
+{
+    public class Parse
+    {
+
+        public IMathElement ParseExpression(string expressionString)//( add ( mul 2 3 ) 3 )
+        {
+            expressionString = expressionString.Replace("(", " ( ").Replace(")", " ) ");
+            string[] splitStr = expressionString.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            int x = 0;
+            IMathElement ex = Help(splitStr, ref x);
+            return ex;
+        }
+        public IMathElement Help(string[] arr, ref int index)//( add ( mul 2 3 ) 3 )
+        {
+            if (arr[index] == "(")
+            {
+                index++;
+                OperatorTypes op = Enum.Parse<OperatorTypes>(arr[index]);
+
+                index++;
+                IMathElement left = Help(arr, ref index);
+                IMathElement right = Help(arr, ref index);
+                Expression expression = new Expression(op, left, right);
+                index++;
+                return expression;
+            }
+            else
+                return new Number(Convert.ToDouble(arr[index++]));
+        }
+    }
+}
