3397965fe1ab58172d9be595df82b05e54a61e4e
efrat-packter <EFRATPACKTER@GMAIL.COM>
2026-03-18 13:42:48 +0200

    add calculator app

--- calculator/Bootstrapper.cs
+++ calculator/Bootstrapper.cs
@@ -6,43 +6,16 @@ namespace calculator
     public class Bootstrapper
     {
 
-
-        private ExpressionFactory _factory;
-        private Tokenizer _tokenizer;
-        private Validation _validation;
-        private Parse _parse;
-        public void Run()
+        public CalculatorApp Intilize()
         {
-            _factory = new ExpressionFactory();
-            _tokenizer = new Tokenizer();
-            _validation = new Validation(_tokenizer);
-            _parse = new Parse(_factory, _validation);
+            ExpressionFactory factory = new ExpressionFactory();
+            Tokenizer tokenizer = new Tokenizer();
+            Validation validation = new Validation(tokenizer);
+            Parse parse = new Parse(factory, validation);
+            IWriter  writer = new Writer();
+            IReader reader = new Reader();  
 
-            string inputExpression = "";
-            while (true)
-            {
-                Console.WriteLine("enter a expressoin");
-                inputExpression = Console.ReadLine();
-                if (string.IsNullOrWhiteSpace(inputExpression) || !_validation.IsValidInput(inputExpression))
-                {
-                    Console.WriteLine("invalid expression, try again");
-                    continue;
-                }
-                break;
-            }
-
-
-            string[] str = _tokenizer.SplitForToken(inputExpression);
-
-            IExpression expression = _parse.InfixToPrefix(str);
-            try
-            {
-            Console.WriteLine(expression.CalcValue());
-                     }
-            catch (DivideByZeroException ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            return new CalculatorApp(tokenizer, validation, parse,writer,reader);
         }
     }
 }
---
+++ calculator/Writer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace calculator
+{
+    public class Writer : IWriter
+    {
+        public void Write(string output)
+        {
+            Console.WriteLine( output);
+        }
+    }
+}
